cd676f66929ada60617613f6a3043fdd2efb395b
Unish <why.unees29@gmail.com>
2025-03-14 18:40:36 +0545

    huge change on 14th March

---
+++ extras/script.js
@@ -0,0 +1,21 @@
+const extras = [
+    {name : "Password Strength Checker", image:"../extras/images/password-strength.png", link:"https://password-strength-checker-jet.vercel.app/"},
+    {name : "Draw Box", image:"../extras/images/draw-box.png", link:"https://canvas-project-rho.vercel.app/"},
+    {name : "Rock Paper Scissors", image:"/extras/images/rock-paper-scissors.png", link:"https://rock-paper-scissors-delta-pink.vercel.app/"},
+    {name : "Networking Ports", image:"../extras/images/networking-ports.png", link:"https://networking-ports.vercel.app/"},
+    {name : "Http Status Codes", image:"../extras/images/status-codes.png", link:"https://status-codes-ten.vercel.app/"},
+    {name : "Currency Convertor", image:"../extras/images/currency-convertor.png", link:"https://currency-drab-seven.vercel.app/"},
+]
+
+const container = document.querySelector("#extras-container");
+
+extras.forEach(item => {
+    const itemElement = document.createElement("a");
+    itemElement.href = item.link;
+    itemElement.classList.add("extras-card");
+    itemElement.innerHTML = `
+        <img src="${item.image}" alt="${item.name}" class="extras-image">
+        <div class="extras-overlay">${item.name}</div>
+    `;
+    container.appendChild(itemElement);
+});
