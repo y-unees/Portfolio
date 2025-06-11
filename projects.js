f06f844aae22d8d8fad733b4c7d51ff2be2248f1
Unish <why.unees29@gmail.com>
2025-06-11 16:47:28 +0545

    Updates of new project

---
+++ projects.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    name: "Fakebook",
+    image: "/extras/images/fakebook.png", 
+    link: "https://fakebook-silk-mu.vercel.app/",
+    description: "A mock social media platform to spread awareness about phishing and credential security. A cybersecurity project."
+  },
+];
+
+const projectContainer = document.querySelector("#projects-container");
+
+projects.forEach(project => {
+  const card = document.createElement("a");
+  card.href = project.link;
+  card.className = "project-card";
+  card.target = "_blank";
+  card.innerHTML = `
+    <img src="${project.image}" alt="${project.name}" class="project-image">
+    <div class="project-content">
+      <div class="project-title">${project.name}</div>
+      <div class="project-description">${project.description}</div>
+    </div>
+  `;
+  projectContainer.appendChild(card);
+});
